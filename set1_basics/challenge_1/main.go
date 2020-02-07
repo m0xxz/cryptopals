4b5dad4509e492c8f7713e62d2fb7abe81b005ec
Zach <zachrog@gmail.com>
2020-02-06 22:02:34 -0800

    Set 1, Challenge 1 - initial implementation w/ unit testing :3

---
+++ set1_basics/challenge_1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+)
+
+// Convert hex to base64, pretty simple.
+func Chal(input string) string {
+	// Per rules, convert to bytes
+	bytes, _ := hex.DecodeString(input)
+	output := base64.StdEncoding.EncodeToString(bytes)
+	return output
+}
+
+func main() {
+	input := "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+	fmt.Println(Chal(input))
+}
