d9d7b58c680231598f3353c754949f85f2b7a1ca
Zach <zachrog@gmail.com>
2020-02-06 23:57:06 -0800

    Yay test script for all the challenges!

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "CryptoPals Challenges!"
+
+# Set 1 Basics
+echo "Running tests for Set 1 Basics..."
+echo "Challenge 1:"
+go test cryptopals/set1_basics/challenge_1
+echo .
+
+echo "Challenge 2:"
+go test cryptopals/set1_basics/challenge_2
+echo .
+
+# More here once I complete those challenges
+
+echo "Done!"
