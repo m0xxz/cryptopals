7556a471be4bfd2b1c2c7e4674ba2c291a6c7da3
Zach <zachrog@gmail.com>
2020-06-22 01:05:27 -0700

    hamming distance seems to be accurate

--- set1_basics/challenge_6/main.go
+++ set1_basics/challenge_6/main.go
@@ -4,9 +4,28 @@ import (
 	"fmt"
 )
 
+// Compute the hamming distance between two byte arrays
+func hamming(a, b []byte) int {
+	if len(a) != len(b) {
+		fmt.Println("[!] Hamming: a,b must be same length!")
+	}
+
+	dist := 0
+	for i := range a {
+		// 8 bits in 1 byte
+		for j := 0; j < 8; j++ {
+			mask := byte(1 << uint(j))
+			if (a[i] & mask) != (b[i] & mask) {
+				dist++
+			}
+		}
+	}
+	return dist
+}
+
 // Break repeating-key XOR
 func chal() string {
-
+	fmt.Println(hamming([]byte("this is a test"), []byte("wokka wokka!!!")))
 	return ""
 }
 
