d0a2686e99c3906928297196047154c95bbff79c
Zach <zachrog@gmail.com>
2020-06-22 00:08:30 -0700

    challenge 5 of set 1 complete <3

---
+++ set1_basics/challenge_5/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+// Repeating-key XOR
+func chal() string {
+	// Opening stanza and our repeating key
+	line := []byte(string("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"))
+	key := []byte(string("ICE"))
+
+	i := 0
+	var encryptedLine []byte
+	for _, b := range line {
+		if i > 2 {
+			i = 0
+		}
+		encryptedLine = append(encryptedLine, byte(b^key[i]))
+		i++
+	}
+
+	return hex.EncodeToString(encryptedLine)
+}
+
+func main() {
+	fmt.Println(chal())
+}
